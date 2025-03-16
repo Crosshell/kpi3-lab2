ccc94d130f59055b400145662f80b6f3f5654f3a
kytaec <vladislavtokovenko4@gmail.com>
2025-03-16 18:47:41 +0200

    Add injection

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -11,53 +11,51 @@ import (
 )
 
 var (
-  inputExpression = flag.String("e", "", "Expression to compute")
-  inputFile       = flag.String("f", "", "Input file containing the expression")
-  outputFile      = flag.String("o", "", "Output file for the result")
+	inputExpression = flag.String("e", "", "Expression to compute")
+	inputFile       = flag.String("f", "", "Input file containing the expression")
+	outputFile      = flag.String("o", "", "Output file for the result")
 )
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  if *inputExpression != "" && *inputFile != "" {
-    fmt.Fprintln(os.Stderr, "Error: Provide either -e or -f, not both.")
-    os.Exit(1)
-  }
+	if *inputExpression != "" && *inputFile != "" {
+		fmt.Fprintln(os.Stderr, "Error: Provide either -e or -f, not both.")
+		os.Exit(1)
+	}
 
-  var inputSource io.Reader
-  var err error
+	var inputSource io.Reader
+	var err error
 
-  if *inputFile != "" {
-    file, err := os.Open(*inputFile)
-    if err != nil {
-      fmt.Fprintln(os.Stderr, "Error opening input file:", err)
-      os.Exit(1)
-    }
-    defer file.Close()
-    inputSource = file
-  } else if *inputExpression != "" {
-    inputSource = strings.NewReader(*inputExpression)
-  } else {
-    inputSource = os.Stdin
-  }
+	if *inputFile != "" {
+		file, err := os.Open(*inputFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error opening input file:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		inputSource = file
+	} else if *inputExpression != "" {
+		inputSource = strings.NewReader(*inputExpression)
+	} else {
+		fmt.Fprintln(os.Stderr, "No input provided, using stdin.")
+		inputSource = os.Stdin
+	}
 
-  var outputDest *os.File = os.Stdout
-  if *outputFile != "" {
-    outputDest, err = os.Create(*outputFile)
-    if err != nil {
-      fmt.Fprintln(os.Stderr, "Error creating output file:", err)
-      os.Exit(1)
-    }
-    defer outputDest.Close()
-  }
+	var outputDest io.Writer = os.Stdout
+	if *outputFile != "" {
+		outputDest, err = os.Create(*outputFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error creating output file:", err)
+			os.Exit(1)
+		}
+		defer outputDest.(*os.File).Close()
+	}
 
-  handler := &lab2.ComputeHandler{
-    Input:  inputSource,
-    Output: outputDest,
-  }
+	handler := lab2.NewComputeHandler(inputSource, outputDest)
 
-  if err := handler.Compute(); err != nil {
-    fmt.Fprintln(os.Stderr, "Computation error:", err)
-    os.Exit(1)
-  }
+	if err := handler.Compute(); err != nil {
+		fmt.Fprintln(os.Stderr, "Computation error:", err)
+		os.Exit(1)
+	}
 }
--- handler.go
+++ handler.go
@@ -1,28 +1,35 @@
 package lab2
 
 import (
-  "fmt"
-  "io"
-  "strings"
+	"fmt"
+	"io"
+	"strings"
 )
 
 type ComputeHandler struct {
-  Input  io.Reader
-  Output io.Writer
+	Input  io.Reader
+	Output io.Writer
+}
+
+func NewComputeHandler(input io.Reader, output io.Writer) *ComputeHandler {
+	return &ComputeHandler{
+		Input:  input,
+		Output: output,
+	}
 }
 
 func (ch *ComputeHandler) Compute() error {
-  buf := new(strings.Builder)
-  _, err := io.Copy(buf, ch.Input)
-  if err != nil {
-    return err
-  }
+	buf := new(strings.Builder)
+	_, err := io.Copy(buf, ch.Input)
+	if err != nil {
+		return err
+	}
 
-  result, err := EvaluatePrefixExpression(buf.String())
-  if err != nil {
-    return err
-  }
+	result, err := EvaluatePrefixExpression(buf.String())
+	if err != nil {
+		return err
+	}
 
-  _, err = fmt.Fprintf(ch.Output, "%f\n", result)
-  return err
+	_, err = fmt.Fprintf(ch.Output, "%f\n", result)
+	return err
 }
