1a532bc12fbe0d1b97a7baa1d7b72270e458fa69
Crosshell <confighell@gmail.com>
2025-03-16 17:35:44 +0200

    Implement prefix expression evaluator Added EvaluatePrefixExpression function to evaluate mathematical expressions in prefix form Supported operators: +, -, *, /, ^ Errors handled: empty expression, invalid token, division by zero, syntax errors

--- implementation.go
+++ implementation.go
@@ -1,9 +1,64 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"math"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func EvaluatePrefixExpression(expression string) (float64, error) {
+	if expression == "" {
+		return 0, errors.New("empty expression")
+	}
+
+	tokens := strings.Fields(expression)
+	if len(tokens) == 0 {
+		return 0, errors.New("invalid expression")
+	}
+
+	stack := []float64{}
+	for i := len(tokens) - 1; i >= 0; i-- {
+		tok := tokens[i]
+
+		switch tok {
+		case "+", "-", "*", "/", "^":
+			if len(stack) < 2 {
+				return 0, errors.New("syntax error: not enough operands")
+			}
+			op1, op2 := stack[len(stack)-1], stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+
+			var res float64
+			switch tok {
+			case "+":
+				res = op1 + op2
+			case "-":
+				res = op1 - op2
+			case "*":
+				res = op1 * op2
+			case "/":
+				if op2 == 0 {
+					return 0, errors.New("division by zero")
+				}
+				res = op1 / op2
+			case "^":
+				res = math.Pow(op1, op2)
+			}
+			stack = append(stack, res)
+
+		default:
+			num, err := strconv.ParseFloat(tok, 64)
+			if err != nil {
+				return 0, errors.New("invalid token: " + tok)
+			}
+			stack = append(stack, num)
+		}
+	}
+
+	if len(stack) != 1 {
+		return 0, errors.New("syntax error: invalid expression")
+	}
+
+	return stack[0], nil
 }
